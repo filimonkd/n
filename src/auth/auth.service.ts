fbd210068a2eee446536dc4b0bd79be692742768
filimonkd <filimonkd@gmail.com>
2023-11-16 21:14:00 +0900

    signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,8 +7,22 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return 'signin';
+  async signin(dto: AuthDto) {
+    //find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    //if user not found throw exp
+    if (!user) {
+      throw new ForbiddenException('credential incorrect');
+    }
+    //compare hash
+    const pwMatchs = await argon.verify(user.hash, dto.password);
+    //if pass dont match throw exp
+    if (!pwMatchs) throw new ForbiddenException('credential incorrect');
+    //send back the user
+    delete user.hash;
+    return user;
   }
   async signup(dto: AuthDto) {
     //hash password
